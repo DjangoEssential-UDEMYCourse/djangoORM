b95dd5015a5c463bffc3d2ec08b0f290a674947d
THUNDER MASTER <thndrmaster@gmail.com>
2021-07-21 09:03:08 -0300

    [AULA 81 - 82] Relacionamento Um para Muitos & Relacionamento Muitos para Muitos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext as _
 
@@ -16,11 +17,35 @@ class Chassi(models.Model):
         return self.numero
 
 
+class Montadora(models.Model):
+    nome = models.CharField(_('Nome'), max_length=100)
+
+    class Meta:
+        verbose_name = _('Montadora')
+        verbose_name_plural = _('Montadoras')
+
+    def __str__(self):
+        return self.nome
+
+
 class Carro(models.Model):
     '''
-    Cada carro só pode se relacionar com um chassi
+    # OneToOneField
+    Cada carro só pode se relacionar com um chassi e um Chassi pode
+    se relacionar apenas com um unico carro
+
+    # ForeignKey (One To Many)
+    Cada carro possui uma montadora, mas uma Montadora pode possuir varios
+    carros
+
+    # Many to Many
+    Um carro pode ser dirigido por vários motoristas e
+    Um motorista pode dirigir diversos carros porem não no
+    mesmo tempo.
     '''
     chassi = models.OneToOneField(Chassi, on_delete=models.CASCADE)
+    montadora = models.ForeignKey(Montadora, on_delete=models.CASCADE)
+    motoristas = models.ManyToManyField(get_user_model())
     modelo = models.CharField(_('Modelo'), max_length=30, help_text=_('Maximo 30 caracteres'))
     preco = models.DecimalField(_('Preço'), max_digits=7, decimal_places=2)
 
@@ -29,5 +54,4 @@ class Carro(models.Model):
         verbose_name_plural = "carros"
 
     def __str__(self):
-        return self.modelo
-
+        return f'{self.montadora} {self.modelo}'
